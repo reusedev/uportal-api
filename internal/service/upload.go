bd8822d167a8c5fc1fc2f53671edcc8994863a61
Cailiang <1006980793@qq.com>
2025-06-30 16:22:23 +0800

    修复 502 bug

--- internal/service/upload.go
+++ internal/service/upload.go
@@ -1,24 +1,27 @@
 package service
 
 import (
+	"os"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"github.com/reusedev/uportal-api/internal/model"
 	"github.com/reusedev/uportal-api/pkg/config"
 	"github.com/reusedev/uportal-api/pkg/errors"
 	"github.com/reusedev/uportal-api/pkg/response"
-	"os"
-	"strconv"
 )
 
 func Upload(c *gin.Context) {
 	file, err := c.FormFile("file")
 	if err != nil {
 		response.Error(c, errors.New(errors.ErrCodeInvalidParams, "无效的请求参数", err))
+		return
 	}
 	os.MkdirAll("tmp", os.ModePerm)
 	filePath := "tmp/" + file.Filename
 	if err = c.SaveUploadedFile(file, filePath); err != nil {
-		response.Error(c, errors.New(errors.ErrCodeInvalidParams, "无效的请求参数", err))
+		response.Error(c, errors.New(errors.ErrCodeInvalidParams, "保存文件失败", err))
+		return
 	}
 	defer func() {
 		os.Remove(filePath) // 删除临时文件
@@ -26,7 +29,8 @@ func Upload(c *gin.Context) {
 	//上传
 	uploadFile, err := model.UploadFile(filePath, config.GlobalConfig.DrawApi.UploadFileUrl)
 	if err != nil {
-		response.Error(c, errors.New(errors.ErrCodeInvalidParams, "无效的请求参数", err))
+		response.Error(c, errors.New(errors.ErrCodeInvalidParams, "上传文件失败", err))
+		return
 	}
 	ret := map[string]interface{}{
 		"id":  strconv.Itoa(uploadFile.Data.Id),
